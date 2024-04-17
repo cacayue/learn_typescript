12e3052d4d75fad64de69f186b53de99fe64d8c4
mafutian <a15453606@outlook.com>
2024-04-17 09:35:27 +0800

    增加url处理

--- ts-axios/src/functions/xhr.ts
+++ ts-axios/src/functions/xhr.ts
@@ -3,7 +3,7 @@ import { AxiosHttpRequestConfig } from "../types";
 export default function xhr(config: AxiosHttpRequestConfig){
   const { data = null, url, method = 'get'} = config;
 
-  var oReq = new XMLHttpRequest();
+  let oReq = new XMLHttpRequest();
   // 打开异步请求
   oReq.open(method, url, true);
   oReq.send();
--- ts-axios/src/index.ts
+++ ts-axios/src/index.ts
@@ -1,8 +1,19 @@
 import xhr from "./functions/xhr";
+import { buildURL } from "./helpers/url";
 import { AxiosHttpRequestConfig } from "./types";
 
 function axios(config: AxiosHttpRequestConfig): void{
+  processConfig(config);
   xhr(config);
+}
+
+function processConfig(config: AxiosHttpRequestConfig):void{
+  config.url = transformUrl(config);
+}
+
+function transformUrl(config: AxiosHttpRequestConfig): string{
+  const { url, params} = config;
+  return buildURL(url, params);
 }
 
 export default axios;
